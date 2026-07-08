bb8bdfb1113a59d3cdfdfa48350de7fc34ffabc9
rupinder0524 <rupindernetsquare@gmail.com>
2026-07-08 15:49:42 +0530

    initial

---
+++ loopexcersice.py
@@ -0,0 +1,108 @@
+# Program 1
+# Print numbers from 1 to 10.
+# for i in  range (1,11) :
+#     print(i)
+
+
+# Program 2
+# Print even numbers from 1 to 20.
+# for i in range(2,21,2):
+#     print(i)
+
+# Program 3
+# Print odd numbers from 1 to 20.
+# for i in range(1,20,2):
+#     print(i)
+
+
+# Program 4
+# Print your name 10 times.
+# for i in  range(1,11):
+#     print("Rupinder ")
+
+
+# Program 5
+# Print all items from a shopping list.
+# shopping_list=["pen","pencil","Eraser","scale"]
+# for i in shopping_list:
+#     print(i)
+
+
+# Program 6
+# Count the number of characters in a string using a loop.
+# Student_name="Raman"
+# count=0
+# for i in Student_name:
+#     count+=1
+#     print(count,i)
+
+
+# Program 7
+# Print the multiplication table of a number entered by the user.
+# num= int(input("Enter Number"))
+# for i in range(1,11):
+#     mul=num*i
+#     print(num,"*",i,"=",mul)
+
+
+#  Program 8
+#  Calculate the sum of numbers from 1 to 100.
+# sum=0
+# for i in range(1,101):
+#     sum+=i
+#     print("sum: ",sum)
+
+
+# Program 9
+# Print all dictionary keys and values.
+# student={"name":"Rupinder",
+#          "grade":"BCA",
+#          "RollNo":24
+# }
+# for key in student:
+#     print(key,":",student[key])
+
+
+# Program 10
+# Print numbers from 20 to 1 in reverse
+# for i in range(20,0,-1):
+#     print(i)    
+
+
+# Program 11
+# Print all multiples of 5 between 1 and 100.
+# for i in range(0,101,5):
+#     print(i)
+
+
+# Program 12
+#Find the sum of all even numbers from 1 to 50
+# sum=0
+# for i in range(2,51,2):
+#     sum+=i
+#     print("sum: ",sum)
+
+
+# Program 13
+#Print each character of your name on a new line.
+# name="Rupinder Kaur"
+# for i in name:
+#     print(i)
+
+
+#Program 14
+# Create a list of 5 cities and print them using a for loop.
+# cities=["jaipur","Ludhiana","Rajkot","Mumbai","Chandigarh"]
+# for i in cities:
+#     print(i)
+
+
+#program 15
+#Create a dictionary of a student and print all keys and values using .items().
+student={"name":"Rupinder",
+         "college":"GNDEC",
+         "RollNo": 24,
+         "Grade": "BCA"}
+for key,value in student.items():
+    print(key,":",value)
+
